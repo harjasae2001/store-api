f0c5c5bc87851e082a31adda7bdce15e4f28650b
Harjas Singh <83627055+harjasae2001@users.noreply.github.com>
2024-08-13 09:58:51 +0530

    chore: Update mongoose connection options for better compatibility
    The code changes modify the `app.js` file to update the options for the `mongoose.connect()` method. The `useNewUrlParser` and `useUnifiedTopology` options are added to ensure better compatibility with the MongoDB driver. This change is necessary to improve the connection setup for the application.

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     app.listen(8080);
     // const io = require('./socket').init(server);
